9fc408d14b88b67d64e25ad51e69fe3f04b0c338
chitalarose <22031@supnum.mr>
2026-04-27 17:40:50 +0000

    Update add_transaction.dart

--- lib/screens/add_transaction.dart
+++ lib/screens/add_transaction.dart
@@ -1,1 +1,62 @@
 
+import 'package:flutter/material.dart';
+
+class AddTransactionPage extends StatelessWidget {
+  const AddTransactionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Ajouter une transaction"),
+        backgroundColor: Colors.teal,
+        foregroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text("Détails de l'opération", 
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            const SizedBox(height: 20),
+            const TextField(
+              decoration: InputDecoration(
+                labelText: "Titre",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 15),
+            const TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: "Montant",
+                prefixText: "MRU ",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 15),
+            DropdownButtonFormField(
+              decoration: const InputDecoration(labelText: "Catégorie", border: OutlineInputBorder()),
+              items: ["Nourriture", "Transport", "Loisirs", "Santé"].map((String category) {
+                return DropdownMenuItem(value: category, child: Text(category));
+              }).toList(),
+              onChanged: (value) {},
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () => Navigator.pop(context),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.teal,
+                minimumSize: const Size(double.infinity, 55),
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+              ),
+              child: const Text("ENREGISTRER", 
+                  style: TextStyle(fontSize: 16, color: Colors.white)), // Placé en dernier !
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
