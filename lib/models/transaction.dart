44dfd8295c1c6d9505d4a4d6e4fbc26da04a0837
Hmahme <Hmahme@gmail.com>
2026-04-29 20:39:01 +0000

    update transaction model with userId

--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -1,5 +1,6 @@
 class Transaction {
   int id;
+  final int userId;
   double montant;
   String type; // revenu ou depense
   String categorie;
@@ -7,6 +8,7 @@ class Transaction {
 
   Transaction({
     required this.id,
+    required this.userId,
     required this.montant,
     required this.type,
     required this.categorie,
